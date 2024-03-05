1cf65a8cff6e64fecd7c54440908d43467a9d350
Emmanuel Munoz <emunozce80@gmail.com>
2024-03-05 13:12:59 -0600

    Add login and sign in buttons to forms

--- frontend/src/components/Login_form.tsx
+++ frontend/src/components/Login_form.tsx
@@ -1,26 +1,30 @@
-import { Card, CardHeader, CardBody, CardFooter, Input, Link } from "@nextui-org/react";
+import { Card, CardHeader, CardBody, CardFooter, Input, Link, Button } from "@nextui-org/react";
+import { useState } from "react";
 
 export default function Login_form() {
+    const [isDisabled, setDisabled] = useState<boolean>(false);
+
     return (
-        <form action="" className="flex justify-center items-center mt-60">
-            <Card className="w-4/5 sm:w-3/5">
+        <form action="" className="flex justify-center items-center">
+            <Card className="w-10/12 md:w-7/12 lg:w-5/12">
                 <CardHeader>
-                    <h4>Login</h4>
+                    <h4 className="text-3xl text-green-500 font-bold">Login</h4>
                 </CardHeader>
                 <CardBody>
-                    <form action="">
+                    <form action="" className="flex flex-col justify-center items-center">
                         <Input
-                            className="py-2"
+                            className="pb-1"
                             isRequired={true}
                             type="email"
                             label="Email"
                             labelPlacement="outside" />
                         <Input
-                            className="pt-2 pr-1"
+                            className="pt-2"
                             isRequired={true}
                             type="password"
                             label="Password"
                             labelPlacement="outside" />
+                        <Button isDisabled={isDisabled} className="w-4/12 mt-7 bg-green-500 font-semibold">Login</Button>
                     </form>
                 </CardBody>
                 <CardFooter>
--- frontend/src/components/Sign_in_form.tsx
+++ frontend/src/components/Sign_in_form.tsx
@@ -1,15 +1,18 @@
-import { Card, CardHeader, CardBody, CardFooter, Input, Link } from "@nextui-org/react";
+import { Card, CardHeader, CardBody, CardFooter, Input, Link, Button } from "@nextui-org/react";
+import { useState } from "react";
 
 export default function Sign_in_form() {
+    const [isDisabled, setDisabled] = useState<boolean>(false);
+
     return (
-        <form action="" className="flex justify-center items-center mt-40">
-            <Card className="w-4/5 sm:w-3/5">
+        <form action="" className="flex justify-center items-center">
+            <Card className="w-10/12 md:w-7/12 lg:w-5/12">
                 <CardHeader>
-                    <h4>Sign In</h4>
+                    <h4 className="text-3xl text-green-500 font-bold">Sign In</h4>
                 </CardHeader>
                 <CardBody>
-                    <form action="">
-                        <div className="flex pb-2">
+                    <form action="" className="flex flex-col justify-center">
+                        <div className="flex flex-row pb-1">
                             <Input
                                 className="pr-1"
                                 isRequired={true}
@@ -23,20 +26,22 @@ export default function Sign_in_form() {
                                 label="Lastname"
                                 labelPlacement="outside" />
                         </div>
+                        <div className="flex flex-row pt-2">
+                            <Input
+                                className="pr-1"
+                                isRequired={true}
+                                type="text"
+                                label="Username"
+                                labelPlacement="outside" />
+                            <Input
+                                className="pl-1"
+                                isRequired={true}
+                                type="email"
+                                label="Email"
+                                labelPlacement="outside" />
+                        </div>
                         <Input
-                            className="py-2"
-                            isRequired={true}
-                            type="text"
-                            label="Username"
-                            labelPlacement="outside" />
-                        <Input
-                            className="py-2"
-                            isRequired={true}
-                            type="email"
-                            label="Email"
-                            labelPlacement="outside" />
-                        <Input
-                            className="py-2"
+                            className="pt-2"
                             isRequired={true}
                             type="password"
                             label="Password"
@@ -47,6 +52,7 @@ export default function Sign_in_form() {
                             type="password"
                             label="Confirm your Password"
                             labelPlacement="outside" />
+                        <Button isDisabled={isDisabled} className="self-center w-4/12 mt-7 bg-green-500 font-semibold">Sign In</Button>
                     </form>
                 </CardBody>
                 <CardFooter>
