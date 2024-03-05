03a215757e4e39002ff1e802d8f4e8a91ec26077
Emmanuel Munoz <emunozce80@gmail.com>
2024-03-05 17:17:02 -0600

    Refactor login form component

--- frontend/src/components/Login_form.tsx
+++ frontend/src/components/Login_form.tsx
@@ -1,34 +1,56 @@
 import { Card, CardHeader, CardBody, CardFooter, Input, Link, Button, Spacer } from "@nextui-org/react";
+import { useForm } from "react-hook-form";
+
+interface Login_form {
+    email: string;
+    password: string;
+}
 
 export default function Login_form() {
+    const { register, handleSubmit } = useForm<Login_form>({
+        defaultValues: {
+            email: "",
+            password: ""
+        }
+    });
+
+    const onSubmit = handleSubmit(data => console.log(data))
+
 
     return (
-        <form action="" className="flex justify-center items-center">
-            <Card className="w-10/12 md:w-7/12 lg:w-5/12">
-                <CardHeader>
-                    <h4 className="text-3xl text-green-500 font-bold">Login</h4>
-                </CardHeader>
-                <CardBody>
-                    <form action="" className="flex flex-col justify-center items-center">
-                        <Input
-                            isRequired={true}
-                            type="email"
-                            label="Email"
-                            labelPlacement="outside" />
-                        <Spacer y={2}></Spacer>
-                        <Input
-                            isRequired={true}
-                            type="password"
-                            label="Password"
-                            labelPlacement="outside" />
-                        <Spacer y={6}></Spacer>
-                        <Button className="w-4/12 bg-green-500 font-semibold">Login</Button>
-                    </form>
-                </CardBody>
-                <CardFooter>
-                    <p>Don't have an account? <Link href="/signin" className="text-green-500 font-semibold">Sign In</Link></p>
-                </CardFooter>
-            </Card>
-        </form >
+        <>
+            <Spacer y={72} ></Spacer >
+            <div className="flex justify-center items-center">
+                <Card className="w-10/12 md:w-7/12 lg:w-5/12">
+                    <CardHeader>
+                        <h4 className="text-3xl text-green-500 font-bold">Login</h4>
+                    </CardHeader>
+                    <CardBody>
+                        <form onSubmit={onSubmit}
+                            className="flex flex-col justify-center items-center">
+                            <Input
+                                {...register("email")}
+                                isRequired={true}
+                                type="email"
+                                label="Email"
+                                labelPlacement="outside" />
+                            <Spacer y={2}></Spacer>
+                            <Input
+                                {...register("password")}
+                                isRequired={true}
+                                type="password"
+                                label="Password"
+                                labelPlacement="outside" />
+                            <Spacer y={6}></Spacer>
+                            <Button type="submit" className="w-4/12 bg-green-500 font-semibold">Login</Button>
+                        </form>
+                    </CardBody>
+                    <CardFooter>
+                        <p>Don't have an account? <Link href="/signin" className="text-green-500 font-semibold">Sign In</Link></p>
+                    </CardFooter>
+                </Card>
+            </div>
+            <Spacer y={72} ></Spacer>
+        </>
     )
 }
--- frontend/src/components/Sign_in_form.tsx
+++ frontend/src/components/Sign_in_form.tsx
@@ -3,60 +3,64 @@ import { Card, CardHeader, CardBody, CardFooter, Input, Link, Button, Spacer } f
 export default function Sign_in_form() {
 
     return (
-        <form action="" className="flex justify-center items-center">
-            <Card className="w-10/12 md:w-7/12 lg:w-5/12">
-                <CardHeader>
-                    <h4 className="text-3xl text-green-500 font-bold">Sign In</h4>
-                </CardHeader>
-                <CardBody>
-                    <form action="" className="flex flex-col justify-center">
-                        <div className="flex flex-row">
+        <>
+            <Spacer y={72} ></Spacer >
+            <div className="flex justify-center items-center">
+                <Card className="w-10/12 md:w-7/12 lg:w-5/12">
+                    <CardHeader>
+                        <h4 className="text-3xl text-green-500 font-bold">Sign In</h4>
+                    </CardHeader>
+                    <CardBody>
+                        <form action="" className="flex flex-col justify-center">
+                            <div className="flex flex-row">
+                                <Input
+                                    isRequired={true}
+                                    type="text"
+                                    label="Name"
+                                    labelPlacement="outside" />
+                                <Spacer x={2} />
+                                <Input
+                                    isRequired={true}
+                                    type="text"
+                                    label="Lastname"
+                                    labelPlacement="outside" />
+                            </div>
+                            <Spacer y={2}></Spacer>
+                            <div className="flex flex-row">
+                                <Input
+                                    isRequired={true}
+                                    type="text"
+                                    label="Username"
+                                    labelPlacement="outside" />
+                                <Spacer x={2} />
+                                <Input
+                                    isRequired={true}
+                                    type="email"
+                                    label="Email"
+                                    labelPlacement="outside" />
+                            </div>
+                            <Spacer y={2}></Spacer>
                             <Input
                                 isRequired={true}
-                                type="text"
-                                label="Name"
+                                type="password"
+                                label="Password"
                                 labelPlacement="outside" />
-                            <Spacer x={2} />
+                            <Spacer y={2}></Spacer>
                             <Input
                                 isRequired={true}
-                                type="text"
-                                label="Lastname"
+                                type="password"
+                                label="Confirm your Password"
                                 labelPlacement="outside" />
-                        </div>
-                        <Spacer y={2}></Spacer>
-                        <div className="flex flex-row">
-                            <Input
-                                isRequired={true}
-                                type="text"
-                                label="Username"
-                                labelPlacement="outside" />
-                            <Spacer x={2} />
-                            <Input
-                                isRequired={true}
-                                type="email"
-                                label="Email"
-                                labelPlacement="outside" />
-                        </div>
-                        <Spacer y={2}></Spacer>
-                        <Input
-                            isRequired={true}
-                            type="password"
-                            label="Password"
-                            labelPlacement="outside" />
-                        <Spacer y={2}></Spacer>
-                        <Input
-                            isRequired={true}
-                            type="password"
-                            label="Confirm your Password"
-                            labelPlacement="outside" />
-                        <Spacer y={6}></Spacer>
-                        <Button className="self-center w-4/12 bg-green-500 font-semibold">Sign In</Button>
-                    </form>
-                </CardBody>
-                <CardFooter>
-                    <p>Already have an account? <Link href="/login" className="text-green-500 font-semibold">Login</Link></p>
-                </CardFooter>
-            </Card>
-        </form >
+                            <Spacer y={6}></Spacer>
+                            <Button className="self-center w-4/12 bg-green-500 font-semibold">Sign In</Button>
+                        </form>
+                    </CardBody>
+                    <CardFooter>
+                        <p>Already have an account? <Link href="/login" className="text-green-500 font-semibold">Login</Link></p>
+                    </CardFooter>
+                </Card>
+            </div >
+            <Spacer y={72} ></Spacer>
+        </>
     )
 }
