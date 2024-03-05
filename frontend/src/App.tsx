9b613ecc0c70d96eb52ced55d1db01daf6b5a1f3
Emmanuel Munoz <emunozce80@gmail.com>
2024-03-05 16:35:54 -0600

    Add react-hook-form dependency and update App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,16 +8,13 @@ function App() {
   const navigate = useNavigate();
 
   return (
-    <div className="bg-slate-600 h-screen">
-
-      < NextUIProvider navigate={navigate} >
-        <Navbar />
-        <Routes>
-          <Route path="/login" element={<Login_Page />} />
-          <Route path="/signin" element={<Sign_in_page />} />
-        </Routes>
-      </NextUIProvider >
-    </div>
+    < NextUIProvider navigate={navigate} >
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login_Page />} />
+        <Route path="/signin" element={<Sign_in_page />} />
+      </Routes>
+    </NextUIProvider >
   )
 }
 
