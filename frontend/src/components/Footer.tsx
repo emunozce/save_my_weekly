33bf9e7ada3caa863d8a30dd7d72548a00449477
Emmanuel Munoz <emunozce80@gmail.com>
2024-03-13 13:19:41 -0600

    Update function to handle edge case

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+    return (
+        <div className="flex items-center justify-center">
+            <h1 className="text-lg font-bold">Footer</h1>
+        </div>
+    )
+}
