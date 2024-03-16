505f5da0edf9ba4dc50afad51f555001f7afc2dc
Emmanuel Munoz <emunozce80@gmail.com>
2024-03-15 23:24:46 -0600

    Update main.tsx to set a minimum height for the main element

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <main className="h-screen dark text-foreground bg-background">
+      <main className="min-h-screen dark text-foreground bg-background">
         <App />
       </main>
     </BrowserRouter>
