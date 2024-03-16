c7f92e799a4304372077e74cefde711c4043e850
Emmanuel Munoz <emunozce80@gmail.com>
2024-03-15 23:53:31 -0600

    Refactor Footer component layout

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,20 +2,20 @@ import { Link, Spacer } from "@nextui-org/react";
 
 export default function Footer() {
     return (
-        <div className="bg-green-500 grid grid-cols-1 grid-rows-3 sm:grid-cols-2 sm:grid-rows-2 w-screen">
-            <div className="row-start-1 sm:col-start-1 self-center justify-self-center flex flex-col items-center justify-center">
-                <h1 className="text-slate-800 font-extrabold text-xl">Features</h1>
+        <div className="bg-green-500 grid grid-cols-1 grid-rows-5 sm:grid-cols-2 sm:grid-rows-3 w-screen">
+            <h1 className="text-slate-800 row-start-1 sm:col-start-1 sm:row-start-1 self-center justify-self-center font-extrabold text-xl">Features</h1>
+            <div className="row-start-2 sm:col-start-1 sm:row-start-2 self-center justify-self-center ">
                 <Spacer y={5}></Spacer>
-                <ul className="flex flex-col items-center justify-center">
+                <ul className="flex flex-col justify-center items-center">
                     <li><Link className="text-slate-900" font-semibold isBlock href="/">Save My Weekly</Link></li>
                     <Spacer y={2}></Spacer>
                     <li><Link className="text-slate-900" font-semibold isBlock href="/">Your Top 10</Link></li>
                 </ul>
             </div>
-            <div className="row-start-2 sm:col-start-2 sm:row-start-1 self-center justify-self-center flex flex-col items-center justify-center">
-                <h1 className="text-slate-800 font-extrabold text-xl">About</h1>
+            <h1 className="text-slate-800 row-start-3 sm:col-start-2 sm:row-start-1 font-extrabold text-xl">About</h1>
+            <div className="row-start-3 sm:col-start-2 sm:row-start-1 self-center justify-self-center ">
                 <Spacer y={5}></Spacer>
-                <ul className="flex flex-col items-center justify-center">
+                <ul className="">
                     <Spacer y={2}></Spacer>
                     <li><Link className="text-slate-900" font-semibold isBlock isExternal href="https://github.com/emunozce/Save_My_Weekly" showAnchorIcon>Github Repo</Link></li>
                     <Spacer y={2}></Spacer>
