c70325699d27085307d3b1e12033ab33463380b7
Emmanuel Munoz <emunozce80@gmail.com>
2024-03-15 23:16:50 -0600

    Update Footer component layout

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Link, Spacer } from "@nextui-org/react";
 export default function Footer() {
     return (
         <div className="bg-green-500 grid grid-cols-1 grid-rows-3 sm:grid-cols-2 sm:grid-rows-2 w-screen">
-            <div className="col-start-1 col-end-2 self-center justify-self-center flex flex-col items-center justify-center">
+            <div className="row-start-1 sm:col-start-1 self-center justify-self-center flex flex-col items-center justify-center">
                 <h1 className="text-slate-800 font-extrabold text-xl">Features</h1>
                 <Spacer y={5}></Spacer>
                 <ul className="flex flex-col items-center justify-center">
@@ -12,7 +12,7 @@ export default function Footer() {
                     <li><Link className="text-slate-900" font-semibold isBlock href="/">Your Top 10</Link></li>
                 </ul>
             </div>
-            <div className="col-start-2 col-end-3 self-center justify-self-center flex flex-col items-center justify-center">
+            <div className="row-start-2 sm:col-start-2 sm:row-start-1 self-center justify-self-center flex flex-col items-center justify-center">
                 <h1 className="text-slate-800 font-extrabold text-xl">About</h1>
                 <Spacer y={5}></Spacer>
                 <ul className="flex flex-col items-center justify-center">
@@ -22,7 +22,7 @@ export default function Footer() {
                     <li><Link className="text-slate-900" font-semibold isBlock isExternal href="https://open.spotify.com/" showAnchorIcon>Official Spotify Website</Link></li>
                 </ul>
             </div>
-            <div className="text-slate-900 row-start-2 row-end-3 col-span-2 self-center justify-self-center">Save My Weekly ©2024 Made by emunozce</div>
+            <div className="text-slate-900 row-start-3 sm:row-start-2 sm:col-span-2 self-center justify-self-center">Save My Weekly ©2024 Made by emunozce</div>
         </div >
     )
 }
