8f349c837131ffc07e117c5474c9ef0ab91f3594
Emmanuel Munoz <emunozce80@gmail.com>
2024-03-20 11:04:24 -0600

    Add API serializers,, and URLs

---
+++ backend/api/serializers.py
@@ -0,0 +1,21 @@
+# serializers.py
+from rest_framework import serializers
+from .models import User
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["name", "lastname", "email", "password"]
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }  # Ensures password is write-only
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["email", "password"]
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }  # Ensures password is write-only
--- backend/savemyweekly/settings.py
+++ backend/savemyweekly/settings.py
@@ -38,11 +38,14 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "api.apps.ApiConfig",  # Add this line to the list of installed apps.
+    "rest_framework",  # Add this line to the list of installed apps.
+    "corsheaders",  # Add this line to the list of installed apps.
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",  # Add this line to the list of middleware.
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -122,3 +125,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Add this line to the list of CORS_ALLOWED_ORIGINS
+]
