33adcc0f24fa46e169227f925a4cd69eb3db64e4
Emmanuel Munoz <emunozce80@gmail.com>
2024-04-23 18:02:08 -0600

    Update Home_Page layout to be responsive and fix styling issues

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,54 +8,94 @@ import Footer from "./components/Footer";
 import { useState } from "react";
 
 export interface UserInfo {
-  name: string;
-  lastname: string;
-  isLoggedIn: boolean;
-  isRemembered: boolean;
+    name: string;
+    lastname: string;
+    isLoggedIn: boolean;
+    isRemembered: boolean;
 }
 
 function App() {
-  const navigate = useNavigate();
-  const [userInfo, setUserInfo] = useState<UserInfo>({
-    name: "",
-    lastname: "",
-    isLoggedIn: false,
-    isRemembered: false
-  });
+    const navigate = useNavigate();
+    const [userInfo, setUserInfo] = useState<UserInfo>({
+        name: "",
+        lastname: "",
+        isLoggedIn: false,
+        isRemembered: false,
+    });
 
-  const handleLogout = () => {
-    userInfo.isRemembered ? localStorage.clear() : sessionStorage.clear(); // Clear storage if user is remembered
-    setUserInfo({ ...userInfo, name: "", lastname: "", isLoggedIn: false, isRemembered: false }); // Delete user info
-  }
+    const handleLogout = () => {
+        userInfo.isRemembered ? localStorage.clear() : sessionStorage.clear(); // Clear storage if user is remembered
+        setUserInfo({
+            ...userInfo,
+            name: "",
+            lastname: "",
+            isLoggedIn: false,
+            isRemembered: false,
+        }); // Delete user info
+    };
 
-  const handleLogin = (name: string, lastname: string, isRemembered: boolean) => {
-    setUserInfo({ ...userInfo, name: name, lastname: lastname, isLoggedIn: true, isRemembered: isRemembered }); // Set user info
-  }
+    const handleLogin = (
+        name: string,
+        lastname: string,
+        isRemembered: boolean
+    ) => {
+        setUserInfo({
+            ...userInfo,
+            name: name,
+            lastname: lastname,
+            isLoggedIn: true,
+            isRemembered: isRemembered,
+        }); // Set user info
+    };
 
-  const isRemembered = () => {
-    if (localStorage.length > 0 && !userInfo.isLoggedIn) {
-      setUserInfo({ ...userInfo, name: localStorage.getItem('name')!, lastname: localStorage.getItem('lastname')!, isLoggedIn: true, isRemembered: true }); // Set user info
-      return
-    } else if (sessionStorage.length > 0 && !userInfo.isLoggedIn) {
-      setUserInfo({ ...userInfo, name: sessionStorage.getItem('name')!, lastname: sessionStorage.getItem('lastname')!, isLoggedIn: true, isRemembered: false }); // Set user info
-      return
-    }
-    return
-  }
+    const isRemembered = () => {
+        if (localStorage.length > 0 && !userInfo.isLoggedIn) {
+            setUserInfo({
+                ...userInfo,
+                name: localStorage.getItem("name")!,
+                lastname: localStorage.getItem("lastname")!,
+                isLoggedIn: true,
+                isRemembered: true,
+            }); // Set user info
+            return;
+        } else if (sessionStorage.length > 0 && !userInfo.isLoggedIn) {
+            setUserInfo({
+                ...userInfo,
+                name: sessionStorage.getItem("name")!,
+                lastname: sessionStorage.getItem("lastname")!,
+                isLoggedIn: true,
+                isRemembered: false,
+            }); // Set user info
+            return;
+        }
+        return;
+    };
 
-  isRemembered() // Check if there is user data in storage at first web page start up
+    isRemembered(); // Check if there is user data in storage at first web page start up
 
-  return (
-    < NextUIProvider navigate={navigate} >
-      <Navbar userInfo={userInfo} handleLogout={handleLogout} />
-      <Routes>
-        <Route path="/" element={<Home_Page userInfo={userInfo} />} />
-        <Route path="/login" element={<Login_Page handleLogin={handleLogin} />} />
-        <Route path="/signup" element={<Sign_up_page />} />
-      </Routes>
-      <Footer />
-    </NextUIProvider >
-  )
+    return (
+        <NextUIProvider navigate={navigate}>
+            <Navbar
+                userInfo={userInfo}
+                handleLogout={handleLogout}
+            />
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Home_Page userInfo={userInfo} />}
+                />
+                <Route
+                    path="/login"
+                    element={<Login_Page handleLogin={handleLogin} />}
+                />
+                <Route
+                    path="/signup"
+                    element={<Sign_up_page />}
+                />
+            </Routes>
+            <Footer />
+        </NextUIProvider>
+    );
 }
 
-export default App
+export default App;
--- frontend/src/components/Home_Page.tsx
+++ frontend/src/components/Home_Page.tsx
@@ -18,8 +18,16 @@ export default function Home_Page({ userInfo }: { userInfo: UserInfo }) {
                             src="discover_weekly.jpg"
                         />
                         <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-                            <p className="text-tiny text-white/80">Want to give your current Discover Weekly playlist another chance?</p>
-                            <Button className=" text-black text-tiny font-semibold" color="success" radius="lg" size="md">
+                            <p className="text-tiny text-white/80">
+                                Want to give your current Discover Weekly
+                                playlist another chance?
+                            </p>
+                            <Button
+                                className=" text-black text-tiny font-semibold"
+                                color="success"
+                                radius="lg"
+                                size="md"
+                            >
                                 Save Playlist
                             </Button>
                         </CardFooter>
@@ -36,8 +44,15 @@ export default function Home_Page({ userInfo }: { userInfo: UserInfo }) {
                             src="artists.png"
                         />
                         <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-                            <p className="text-tiny text-white/80">It doesn't get any better than this.</p>
-                            <Button className=" text-black text-tiny font-semibold" color="success" radius="lg" size="md">
+                            <p className="text-tiny text-white/80">
+                                It doesn't get any better than this.
+                            </p>
+                            <Button
+                                className=" text-black text-tiny font-semibold"
+                                color="success"
+                                radius="lg"
+                                size="md"
+                            >
                                 See Top Artists
                             </Button>
                         </CardFooter>
@@ -54,17 +69,26 @@ export default function Home_Page({ userInfo }: { userInfo: UserInfo }) {
                             src="The_New_Abnormal.jpeg"
                         />
                         <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-                            <p className="text-tiny text-white/80">Find out which are the songs you can't get enough of. </p>
-                            <Button className=" text-black text-tiny font-semibold" color="success" radius="lg" size="md">
+                            <p className="text-tiny text-white/80">
+                                Find out which are the songs you can't get
+                                enough of.
+                            </p>
+                            <Button
+                                className=" text-black text-tiny font-semibold"
+                                color="success"
+                                radius="lg"
+                                size="md"
+                            >
                                 See Top Songs
                             </Button>
                         </CardFooter>
                     </Card>
-                </div >
-            )
-                : (
-                    <div className="flex justify-center items-center">Hola, logueate</div>
-                )}
+                </div>
+            ) : (
+                <div className="flex justify-center items-center">
+                    Hola, logueate
+                </div>
+            )}
         </>
-    )
+    );
 }
